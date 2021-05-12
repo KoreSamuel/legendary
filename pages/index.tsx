f969914e3793febbe1fe31486837cc93e3c75a3e
koresamuel <swustxiaojie@163.com>
2021-05-12 22:49:45 +0800

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from 'react';
+import { GetServerSideProps } from 'next';
+import Head from 'next/head';
+
+import { YuqueAPI, IHelloMessage } from '../utils/yuque-api'
+
+interface HomePageProps {
+  hello: IHelloMessage
+}
+const HomePage: FC<HomePageProps> = ({ hello }: HomePageProps) => (
+  <>
+    <Head>
+      <title>home page</title>
+    </Head>
+    <div>
+      <h2>{hello.message}</h2>
+    </div>
+  </>
+)
+
+export default HomePage;
+
+export const getServerSideProps: GetServerSideProps = async (): Promise<{props: HomePageProps}> => {
+  const api = new YuqueAPI(process.env.yuqueToken);
+
+  const { data: hello } = await api.hello();
+  console.log(hello);
+
+  return {
+    props: {
+      hello
+    }
+  }
+}
---
+++ utils/yuque-api.ts
@@ -0,0 +1,43 @@
+const API_ROOT = 'https://www.yuque.com/api/v2';
+
+export interface YuquePayload<T> {
+  data: T;
+}
+
+export interface IHelloMessage {
+  message: string;
+}
+
+
+export class YuqueAPI {
+  private token: string;
+
+  private headers: { [key: string]: string };
+
+  constructor(token: string) {
+    this.token = token;
+    this.headers = {
+      'Content-Type': 'application/json',
+      'User-Agent': 'yuque-kore',
+      'X-Auth-Token': this.token
+    };
+  }
+
+  public async hello(): Promise<YuquePayload<IHelloMessage>> {
+    const { data } = await this.getResult<IHelloMessage>('/hello');
+    return {
+      data
+    }
+  }
+
+  private async getResult<T>(path: string, options: RequestInit = {}): Promise<YuquePayload<T>> {
+    const response = await fetch(`${API_ROOT}${path}`, {
+      method: 'GET',
+      headers: this.headers,
+      ...options
+    });
+
+    return response.json();
+  }
+
+}
